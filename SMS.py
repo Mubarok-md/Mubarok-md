5f86d2b8a6e144bce241703a44ec7228dd6bfb95
Mubarok-md <82583281+Mubarok-md@users.noreply.github.com>
2021-05-20 18:08:49 +0600

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -14,16 +14,7 @@ cyan  = "\033[96m"
 end = '\033[0m'
 line=yellow+"======================================================================================================================"
 space=" "
-logo=red+str("""
-     8888888b.   .d88888b.   .d8888b.       Y88b   d88P 
-     888   Y88b d88P" "Y88b d88P  Y88b       Y88b d88P  
-     888    888 888     888 888    888        Y88o88P   
-     888   d88P 888     888 888        888888  Y888P    
-     8888888P"  888     888 888        888888  d888b    
-     888 T88b   888     888 888    888        d88888b   
-     888  T88b  Y88b. .d88P Y88b  d88P       d88P Y88b  
-     888   T88b  "Y88888P"   "Y8888P"       d88P   Y88b                                                      
-""")
+logo =⸎ ⸎⸎ ⸎⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎⸎ ⸎ ⸎ ⸎ ⸎ ⸎⸎ ⸎ ⸎⸎ ⸎⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎⸎ ⸎ ⸎⸎⸎Hacker-Boys⸎⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎⸎ ⸎⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎⸎ ⸎⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎ ⸎⸎ ⸎ ⸎ ⸎ ⸎ ⸎⸎
 
       
  #HEADER                
@@ -121,7 +112,7 @@ while tt<2:
 	elif opt2=="3":
 		os.system("python3 main2.py")
 	else:
-		text=cyan+"\t\tDeveloped By : Hacker Boy "+green+"\n\n\t\t★★ ROC-X BD SMS Bomber ★★   \n" 
+		text=cyan+"\t\tDeveloped By : Hacker-Boy "+green+"\n\n\t\t★★ ROC-X BD SMS Bomber ★★   \n" 
 		notice=red+"\n\t\t[×] Wrong Value Entered"
 		os.system('clear')
 		header()
